743d86c320ba310258771018f5672cb270f84b4a
p0p3ye <devp0p3yee@gmail.com>
2024-12-20 09:40:12 -0500

    Add general keyshare

--- src/FairyringContract.sol
+++ src/FairyringContract.sol
@@ -6,12 +6,22 @@ import {Context} from "@openzeppelin/contracts/utils/Context.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract FairyringContract is IFairyringContract, Context, Ownable {
+
+    event RequestGeneralID(address indexed requester, uint256 id);
+    event RequestGeneralKey(address indexed requester, uint256 id);
+
+    uint256 generalRequestFee = 0;
     
     bytes public latestEncryptionKey;
     mapping(bytes => bool) public encryptionKeyExists;
 
     mapping(uint256 => bytes) decryptionKeys;
     uint256 latestDecryptionKeyHeight = 0;
+
+    mapping(address =>  uint256) public addressGeneralID;
+    mapping(address => mapping(uint256 => bool)) public generalIDRequested;
+    mapping(address => mapping(uint256 => bool)) public generalKeyRequested;
+    mapping(address => mapping(uint256 => bytes)) public generalDecryptionKeys;
 
     constructor() Ownable(_msgSender()) {}
 
@@ -34,6 +44,40 @@ contract FairyringContract is IFairyringContract, Context, Ownable {
         decryptionKeys[height] = decryptionKey;
         latestDecryptionKeyHeight = height;
     }
+
+    function submitGeneralDecryptionKeys(
+        address requester,
+        uint256 id,
+        bytes memory decryptionKey
+    ) external onlyOwner() {
+        require(generalIDRequested[requester][id], "The given requester & ID have not requested the general identity");
+        require(generalKeyRequested[requester][id], "The given requester & ID have not requested the general decryption key");
+        require(generalDecryptionKeys[requester][id].length == 0, "Decryption key for the given requester & ID already exists");
+        
+        generalDecryptionKeys[requester][id] = decryptionKey;
+
+        // TODO: Do a contract callback with the decryption key here ?
+    }
+
+    // TODO: Update the allow the requester to set a target contract address 
+    // and function to do callback when the decryption key received
+    function requestGeneralDecryptionKey(uint256 id) external {
+        require(generalIDRequested[_msgSender()][id], "Given ID is not requested");
+        require(!generalKeyRequested[_msgSender()][id], "Already request decryption key for the given ID");
+        generalKeyRequested[_msgSender()][id] = true;
+        emit RequestGeneralKey(_msgSender(), id);
+    }
+
+    function requestGeneralID() external {
+        uint256 id = addressGeneralID[_msgSender()];
+        generalIDRequested[_msgSender()][id] = true;
+        addressGeneralID[_msgSender()] = addressGeneralID[_msgSender()] + 1;
+        emit RequestGeneralID(_msgSender(), id);
+    }
+
+    function setRequestGeneralFee(uint256 newFee) external onlyOwner() {
+        generalRequestFee = newFee;
+    }   
 
     function getRandomnessByHeight(uint256 height) external view returns (bytes32) {
         return _getDecryptionKey(height);
